5db28b22a6d91e6acd48c3ae74d72240d6f91a2e
Tristan <75189508+realTristan@users.noreply.github.com>
2021-08-03 12:03:36 -0400

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,38 +1,18 @@
-import autoit
-from time import sleep
+import autoit, os, time, random, sys, chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-import colorama
-from colorama import Fore, init, Style, Back
-import chromedriver_autoinstaller
 from selenium.common.exceptions import NoSuchElementException
-import os
-import time
-import random
-import sys
+from colorama import Fore, init, Style, Back
 init(convert=True)
 
-
-
-
-print(Style.BRIGHT + Fore.BLUE + """
-██╗███╗   ██╗███████╗████████╗ █████╗     ███╗   ███╗███████╗███╗   ███╗███████╗██████╗ 
-██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗    ████╗ ████║██╔════╝████╗ ████║██╔════╝██╔══██╗
-██║██╔██╗ ██║███████╗   ██║   ███████║    ██╔████╔██║█████╗  ██╔████╔██║█████╗  ██████╔╝
-██║██║╚██╗██║╚════██║   ██║   ██╔══██║    ██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║██╔══╝  ██╔══██╗
-██║██║ ╚████║███████║   ██║   ██║  ██║    ██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║███████╗██║  ██║
-╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝    ╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
-                                                                                        """)
-print(Style.BRIGHT + Fore.WHITE + "[+] CODED BY tristan#2230")
-print()
-print(Style.BRIGHT + Fore.BLUE + "[+] INSTALLING REQUIRED MATERIALS")
+# installing dependencies
 print(Style.DIM + Fore.BLACK)
 os.system('pip install chromedriver-autoinstaller')
 os.system('pip install selenium')
 os.system('cls' if os.name == 'nt' else "printf '\033c'")
-
 chromedriver_autoinstaller.install()
+
 
 print(Style.BRIGHT + Fore.BLUE + """
 ██╗███╗   ██╗███████╗████████╗ █████╗     ███╗   ███╗███████╗███╗   ███╗███████╗██████╗ 
@@ -42,121 +22,48 @@ print(Style.BRIGHT + Fore.BLUE + """
 ██║██║ ╚████║███████║   ██║   ██║  ██║    ██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║███████╗██║  ██║
 ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝    ╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
                                                                                         """)
-print(Style.BRIGHT + Fore.WHITE + '[+] CREATE A FOLDER NAMED "instameme" ON YOUR DESKTOP')
-print()
-print('[+] RENAME EACH IMAGE TO "image.jpg"')
-username = open("username.txt", "r")
-username.readlines()
-password = open("password.txt", "r")
-password.readlines()
-caption = open("caption.txt", "r")
-caption.readlines()
-image_path = r"instameme.jpg" #The written path is just an example, Delete the path and Enter the Path of your image. #1. path should not start with a back slash
 print(Fore.BLACK + Style.DIM)
-mobile_emulation = { "deviceName": "Pixel 2" }
+
 opts = webdriver.ChromeOptions()
-opts.add_experimental_option("mobileEmulation", mobile_emulation)
-#opts.add_argument("--headless")
-opts.add_argument('--log-level=3')
+opts.add_experimental_option("mobileEmulation", { "deviceName": "Pixel 2" }); opts.add_argument('--log-level=3'); opts.add_argument("--headless")
+driver = webdriver.Chrome(executable_path=r"chromedriver.exe",options=opts); driver.get("https://www.instagram.com")
 
-driver = webdriver.Chrome(executable_path=r"chromedriver.exe",options=opts)
+def main():
+    #Logging into the account
+    driver.implicitly_wait(3); driver.find_element_by_xpath("//button[contains(text(),'Log In')]").click()
+    driver.implicitly_wait(3); driver.find_element_by_xpath("//input[@name='username']").send_keys(open("username.txt", "r").readline())
+    driver.implicitly_wait(3); driver.find_element_by_xpath("//input[@name='password']").send_keys(open("password.txt", "r").readline()).submit()
 
-main_url = "https://www.instagram.com"
-driver.get(main_url)
-
-sleep(1.5)
-
-def login():
-    login_button = driver.find_element_by_xpath("//button[contains(text(),'Log In')]")
-    login_button.click()
-    sleep(2)
-    username_input = driver.find_element_by_xpath("//input[@name='username']")
-    username_input.send_keys(username)
-    password_input = driver.find_element_by_xpath("//input[@name='password']")
-    password_input.send_keys(password)
-    password_input.submit()
-
-login()
-sleep(1)
-
-def close_reactivated():
     try:
-        sleep(1.5)
-        not_now_btn = driver.find_element_by_xpath("//a[contains(text(),'Not Now')]")
-        not_now_btn.click()
-        
+        driver.implicitly_wait(2); driver.find_element_by_xpath("//a[contains(text(),'Not Now')]").click()
     except:
-        pass
+        driver.implicitly_wait(2); driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
 
-close_reactivated()
-
-def close_notification():
-    try: 
-        sleep(1)
-        close_noti_btn = driver.find_element_by_xpath("//button[contains(text(),'Not Now')]")
-        close_noti_btn.click()
-    except:
-        pass
-
-close_notification()
-
-def close_add_to_home():
     try:
-        sleep(1) 
-        close_addHome_btn = driver.find_element_by_xpath("//button[contains(text(),'Cancel')]")
-        close_addHome_btn.click()
-        sleep(1)
-    except NoSuchElementException: 
-        print() 
-        print(Style.BRIGHT + Fore.RED + "[ERROR] FAILED TO LOGIN")
-        print()
-        print(Fore.RED + "[-] CLOSING PROGRAM", end = "")
-        time.sleep(0.4)
-        print(".", end = "")
-        time.sleep(0.4)
-        print(".", end = "")
-        time.sleep(0.4)
-        print(".", end = "")
-        time.sleep(0.4)
-        sys.exit()
-    else:
-        print(Style.BRIGHT + Fore.GREEN + "[LOG] SUCCESSFULLY LOGGED IN")
-        print(Style.BRIGHT + Fore.BLUE) 
-
-close_add_to_home()
-
-sleep(2)
-
-close_notification()
-
-new_post_btn = driver.find_element_by_xpath("//div[@role='menuitem']").click()
-sleep(1.5)
-autoit.win_active("Open") 
-sleep(2)
-autoit.control_send("Open","Edit1",image_path) 
-sleep(1.5)
-autoit.control_send("Open","Edit1","{ENTER}")
-
-sleep(2)
+        driver.implicitly_wait(5); driver.find_element_by_xpath("//button[contains(text(),'Cancel')]").click()
+    except:
+        print(Style.BRIGHT + Fore.RED + "[LOG] FAILED TO LOG IN" + Fore.BLUE)
 
 
-next_btn = driver.find_element_by_xpath("//button[contains(text(),'Next')]").click()
+    driver.implicitly_wait(2); driver.find_element_by_xpath("//div[@role='menuitem']").click()
+    driver.implicitly_wait(2); autoit.win_active("Open")
+    driver.implicitly_wait(2); autoit.control_send("Open","Edit1", "instameme.jpg")
+    driver.implicitly_wait(2); autoit.control_send("Open","Edit1","{ENTER}")
 
-sleep(1.5)
+    driver.implicitly_wait(2); driver.find_element_by_xpath("//button[contains(text(),'Next')]").click()
+    driver.implicitly_wait(2); driver.find_element_by_xpath("//textarea[@aria-label='Write a caption…']").send_keys(open("caption.txt", "r").readlines())
 
-caption_field = driver.find_element_by_xpath("//textarea[@aria-label='Write a caption…']")
-caption_field.send_keys(caption)
+    driver.implicitly_wait(2); driver.find_element_by_xpath("//button[contains(text(),'Share')]").click()
+
+
+    time.sleep(10); print(Style.BRIGHT + Fore.GREEN + "[LOG] SUCCESSFULLY UPLOADED IMAGE")
+    input(); driver.close()
 
 
 
-share_btn = driver.find_element_by_xpath("//button[contains(text(),'Share')]").click()
 
 
-sleep(10)
-
-print(Style.BRIGHT + Fore.GREEN + "[LOG] SUCCESSFULLY UPLOADED IMAGE")
-
-print(Style.BRIGHT + Fore.BLUE)
-input("[+] PRESS ENTER TO CLOSE THE PROGRAM: ")
-
-driver.close()
+# NOTE: I did some extra cleaning up and changes without fully testing them as I didn't have the time. Therefore if there's any errors or problems,
+# PLEASE DM me on discord @ tristan#2230
+# CHANGE the image you want to upload to instameme.jpg
+# IF your internet is slow, then change the "driver.implicitly_wait(2)" to "driver.implicitly_wait(4)"
