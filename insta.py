9b6c8b4b2c6657f8efa699b8ca0d4b633a896741
Tristan <75189508+realTristan@users.noreply.github.com>
2021-09-07 21:55:12 -0400

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,10 +1,6 @@
-import autoit, os, time, random, sys, chromedriver_autoinstaller
+import autoit, os, time, chromedriver_autoinstaller
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
-from colorama import Fore, init, Style, Back
-init(convert=True)
+from colorama import Fore, Style, init; init(convert=True)
 
 # installing dependencies
 print(Style.DIM + Fore.BLACK)
@@ -15,13 +11,17 @@ chromedriver_autoinstaller.install()
 
 
 print(Style.BRIGHT + Fore.BLUE + """
-██╗███╗   ██╗███████╗████████╗ █████╗     ███╗   ███╗███████╗███╗   ███╗███████╗██████╗ 
-██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗    ████╗ ████║██╔════╝████╗ ████║██╔════╝██╔══██╗
-██║██╔██╗ ██║███████╗   ██║   ███████║    ██╔████╔██║█████╗  ██╔████╔██║█████╗  ██████╔╝
-██║██║╚██╗██║╚════██║   ██║   ██╔══██║    ██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║██╔══╝  ██╔══██╗
-██║██║ ╚████║███████║   ██║   ██║  ██║    ██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║███████╗██║  ██║
-╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝    ╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
-                                                                                        """)
+ █████                     █████                                                          
+░░███                     ░░███                                                           
+ ░███  ████████    █████  ███████    ██████    ███████ ████████   ██████   █████████████  
+ ░███ ░░███░░███  ███░░  ░░░███░    ░░░░░███  ███░░███░░███░░███ ░░░░░███ ░░███░░███░░███ 
+ ░███  ░███ ░███ ░░█████   ░███      ███████ ░███ ░███ ░███ ░░░   ███████  ░███ ░███ ░███ 
+ ░███  ░███ ░███  ░░░░███  ░███ ███ ███░░███ ░███ ░███ ░███      ███░░███  ░███ ░███ ░███ 
+ █████ ████ █████ ██████   ░░█████ ░░████████░░███████ █████    ░░████████ █████░███ █████
+░░░░░ ░░░░ ░░░░░ ░░░░░░     ░░░░░   ░░░░░░░░  ░░░░░███░░░░░      ░░░░░░░░ ░░░░░ ░░░ ░░░░░ 
+                                              ███ ░███                                    
+                                             ░░██████                                     
+                                              ░░░░░░                                      """)
 print(Fore.BLACK + Style.DIM)
 
 opts = webdriver.ChromeOptions()
@@ -29,21 +29,15 @@ opts.add_experimental_option("mobileEmulation", { "deviceName": "Pixel 2" }); op
 driver = webdriver.Chrome(executable_path=r"chromedriver.exe",options=opts); driver.get("https://www.instagram.com")
 
 def main():
-    #Logging into the account
     driver.implicitly_wait(3); driver.find_element_by_xpath("//button[contains(text(),'Log In')]").click()
     driver.implicitly_wait(3); driver.find_element_by_xpath("//input[@name='username']").send_keys(open("username.txt", "r").readline())
     driver.implicitly_wait(3); driver.find_element_by_xpath("//input[@name='password']").send_keys(open("password.txt", "r").readline()).submit()
 
-    try:
-        driver.implicitly_wait(2); driver.find_element_by_xpath("//a[contains(text(),'Not Now')]").click()
-    except:
-        driver.implicitly_wait(2); driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
+    try: driver.implicitly_wait(2); driver.find_element_by_xpath("//a[contains(text(),'Not Now')]").click()
+    except: driver.implicitly_wait(2); driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
 
-    try:
-        driver.implicitly_wait(5); driver.find_element_by_xpath("//button[contains(text(),'Cancel')]").click()
-    except:
-        print(Style.BRIGHT + Fore.RED + "[LOG] FAILED TO LOG IN" + Fore.BLUE)
-
+    try: driver.implicitly_wait(5); driver.find_element_by_xpath("//button[contains(text(),'Cancel')]").click()
+    except: print(Style.BRIGHT + Fore.RED + "[LOG] FAILED TO LOG IN" + Fore.BLUE)
 
     driver.implicitly_wait(2); driver.find_element_by_xpath("//div[@role='menuitem']").click()
     driver.implicitly_wait(2); autoit.win_active("Open")
@@ -52,13 +46,9 @@ def main():
 
     driver.implicitly_wait(2); driver.find_element_by_xpath("//button[contains(text(),'Next')]").click()
     driver.implicitly_wait(2); driver.find_element_by_xpath("//textarea[@aria-label='Write a caption…']").send_keys(open("caption.txt", "r").readlines())
-
     driver.implicitly_wait(2); driver.find_element_by_xpath("//button[contains(text(),'Share')]").click()
 
-
-    time.sleep(10); print(Style.BRIGHT + Fore.GREEN + "[LOG] SUCCESSFULLY UPLOADED IMAGE")
-    input(); driver.close()
-
+    time.sleep(10); driver.close()
 
 
 
