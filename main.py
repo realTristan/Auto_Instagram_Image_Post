89580a7c41e930c12f8c37867dc8296729b4822b
Tristan <75189508+realTristan@users.noreply.github.com>
2021-09-07 22:07:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ print(Style.BRIGHT + Fore.BLUE + """
 
 image_name = "image_1.jpg" # // Change "image_1.jpg" to the name of the picture you want to post
 wait_time = 2 # // Make this higher if you have bad internet
+username = "Your Instagram Username"
+password = "Your Instagram Password"
+caption = "The Post Caption"
 
 opts = webdriver.ChromeOptions()
 opts.add_experimental_option("mobileEmulation", { "deviceName": "Pixel 2" }); opts.add_argument('--log-level=3'); opts.add_argument("--headless")
@@ -25,8 +28,8 @@ driver = webdriver.Chrome(executable_path=r"chromedriver.exe",options=opts); dri
 
 def main():
     driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//button[contains(text(),'Log In')]").click()
-    driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//input[@name='username']").send_keys(open("username.txt", "r").readline())
-    driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//input[@name='password']").send_keys(open("password.txt", "r").readline()).submit()
+    driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//input[@name='username']").send_keys(username)
+    driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//input[@name='password']").send_keys(password).submit()
 
     try: driver.implicitly_wait(3); driver.find_element_by_xpath("//a[contains(text(),'Not Now')]").click()
     except: driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
@@ -40,7 +43,10 @@ def main():
     driver.implicitly_wait(wait_time); autoit.control_send("Open","Edit1","{ENTER}")
 
     driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//button[contains(text(),'Next')]").click()
-    driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//textarea[@aria-label='Write a caption…']").send_keys(open("caption.txt", "r").readlines())
+    driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//textarea[@aria-label='Write a caption…']").send_keys(caption)
     driver.implicitly_wait(wait_time); driver.find_element_by_xpath("//button[contains(text(),'Share')]").click()
 
     time.sleep(10); driver.close()
+
+if __name__ == "__main__":
+    main()
